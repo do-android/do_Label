f84cf4894e59103b3fd467546bc41bcda4ef247d
chenqf <feng_er@outlook.com>
2017-12-18 15:40:55 +0800

    http://jira.deviceone.net:8088/browse/PRODDEV-5826

--- doExt_do_Label/src/doext/implement/do_Label_View.java
+++ doExt_do_Label/src/doext/implement/do_Label_View.java
@@ -151,9 +151,9 @@ public class do_Label_View extends TextView implements DoIUIModuleView, do_Label
 		if (_changedValues.containsKey("padding")) {
 			String _padding = _changedValues.get("padding");
 			String[] _paddings = _padding.split(",");
-			int paddingLeft = (int) (DoTextHelper.strToInt(_paddings[3], 0) * model.getXZoom());
+			int paddingLeft = (int) (DoTextHelper.strToInt(_paddings[1], 0) * model.getXZoom());
 			int paddingTop = (int) (DoTextHelper.strToInt(_paddings[0], 0) * model.getYZoom());
-			int paddingRight = (int) (DoTextHelper.strToInt(_paddings[1], 0) * model.getXZoom());
+			int paddingRight = (int) (DoTextHelper.strToInt(_paddings[3], 0) * model.getXZoom());
 			int paddingBottom = (int) (DoTextHelper.strToInt(_paddings[2], 0) * model.getYZoom());
 			this.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
 		}
